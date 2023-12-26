efa28d0d5fd5cbe0dcfb399bbd25913a636417fb
zaidmunir11s <zaidmunir11s@gmail.com>
2023-12-26 16:15:48 -0500

    feat: added database services

--- db.service.js/db.conn.js
+++ db.service.js/db.conn.js
@@ -1,6 +1,34 @@
 const mysql = require("mysql2/promise");
-const sql = require("./queries.service")
-// Create a connection pool
+const sql = require("./queries.service");
+
+// Database creation and table creation module
+
+async function createDatabase() {
+    try {
+        const connection = await mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "",
+        });
+
+        // Create the database if it does not exist
+        await connection.query(sql.CREATE_DATABASE);
+        console.log("Database created or already exists");
+
+        // Switch to the newly created or existing database
+        await connection.query("USE ems");
+
+        // Create the table if it does not exist
+        await connection.query(sql.CREATE_TABLE_USERS);
+        console.log("Tables created or already exist");
+
+        await connection.end();
+    } catch (error) {
+        console.error("Error creating database or table:", error.message);
+    }
+}
+
+// Connection pool module
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
@@ -8,40 +36,5 @@ const pool = mysql.createPool({
     database: "ems",
 });
 
-module.exports = {
-    async createDatabase() {
-        try {
-            const connection = await mysql.createConnection({
-                host: "localhost",
-                user: "root",
-                password: "",
-            });
-
-            // Create the database if it does not exist
-            await connection.query(sql.CREATE_DB);
-
-            console.log("Database created or already exists");
-            await connection.end();
-        } catch (error) {
-            console.error("Error creating database:", error.message);
-        }
-    }
-
-}
-// Function to create the database if it does not exist
-
-// // Function to connect to the database
-// export async function connectToDatabase() {
-//     try {
-//         // Create the database if it does not exist
-//         await createDatabase();
-
-//         // Use the connection pool to get a connection
-//         const connection = await pool.getConnection();
-
-//         console.log("Connected to the database");
-//         connection.release();
-//     } catch (error) {
-//         console.error("Error connecting to the database:", error.message);
-//     }
-// }
+// Export both modules
+module.exports = { createDatabase, pool };
--- db.service.js/queries.service.js
+++ db.service.js/queries.service.js
@@ -1,5 +1,31 @@
 module.exports = {
-    CREATE_DB: `CREATE DATABASE IF NOT EXISTS ems`
+
+
+    CREATE_DATABASE: `CREATE DATABASE IF NOT EXISTS ems`,
+
+
+    CREATE_TABLE_USERS: `CREATE TABLE IF NOT EXISTS  users (
+        user_id INT AUTO_INCREMENT PRIMARY KEY,
+        profile_picture VARCHAR(150),
+        first_name VARCHAR(50),
+        last_name VARCHAR(50),
+        email VARCHAR(100),
+        password VARCHAR(255),
+        role VARCHAR(10),
+        designation VARCHAR(20),
+        date_of_joining DATE
+    );`,
+
+
+    INSERT_INTO_USERS: `INSERT INTO users
+    (profile_picture, first_name, last_name, email, password, role, designation, date_of_joining)
+     VALUES (?,?,?,?,?,?,?,?)`,
+
+
+    CHECK_USER_REGISTERED: `
+    SELECT email, role 
+    FROM users 
+    WHERE email=? AND role=?`,
 
 }
 
