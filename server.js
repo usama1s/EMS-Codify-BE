57dc2cbef1c90cc7fb94cc3c2404a776e9bd1a96
zaidmunir11s <zaidmunir11s@gmail.com>
2023-12-26 16:15:13 -0500

    running server file

--- server.js
+++ server.js
@@ -10,23 +10,27 @@ dotenv.config();
 
 const app = express();
 
+
+// APP USES
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(fileUpload());
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//CREATE DB IF NOT CREATED
+// CREATE DB IF NOT CREATED
 db.createDatabase();
+
 
 // ROUTE
 app.use('/api', index);
 
 
+// RUN ON PORT OR ANY STATIC IP GIVEN IN ".env"
 const port = process.env.PORT || 5000;
 const ipAddress = process.env.IP_ADDRESS || 'localhost';
+
 
 app.listen(port, ipAddress, () => {
   console.log(`Server is running on http://${ipAddress}:${port}`);
