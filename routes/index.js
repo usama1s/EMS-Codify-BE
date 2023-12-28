94fb2f61e1b3f2e980e4f6a09706c7a0e6adaf02
zaidmunir11s <zaidmunir11s@gmail.com>
2023-12-28 13:44:22 -0500

    feat: added manager files

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-
-
+// ALL ROUTES THAT ARE REQUIRED
 const authRoute = require('./auth.route');
+const attendenceRoute = require('./attendance.route');
+const managerRoute = require('./attendance.route');
 
 
+
+// ROUTES
 router.use('/auth', authRoute);
+router.use('/attendence', attendenceRoute);
+router.use('/manager', managerRoute);
+
 
 
 module.exports = router;
